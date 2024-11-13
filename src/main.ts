ea78e9e6b3cc5e313a1e1805840e98c50ed0dbbd
Codie Sharp <codie.sharp@gmail.com>
2024-11-13 13:44:32 -0500

    bounding box calculations for camera. very distorted now but objects always fit

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import './style.css';
 // import * as constants from './utils/constants';
 import * as shapes from './scenes/createShapes';
 import { ObjectManager } from './utils/objectManager'; 
-import { Engine, Scene, FreeCamera, Camera, Vector3, HemisphericLight, Mesh } from '@babylonjs/core';
+import { Engine, Scene, Camera, Vector3, HemisphericLight, Mesh, TargetCamera } from '@babylonjs/core';
 
 const canvas = document.getElementById('app') as HTMLCanvasElement;
 const engine = new Engine(canvas);
 const scene = new Scene(engine);
 
-const camera = new FreeCamera("camera", new Vector3(0 ,0, -2), scene);
+const camera = new TargetCamera("camera", new Vector3(0 ,0, -2), scene);
 camera.mode = Camera.ORTHOGRAPHIC_CAMERA;
 scene.activeCamera = camera;
 
@@ -16,7 +16,7 @@ let obj1: Mesh = shapes.createCube(scene);
 let obj2: Mesh = shapes.createSphere(scene);
 let obj3: Mesh = shapes.createTorus(scene);
 
-const objectManager = new ObjectManager(engine, camera, obj1, obj2, obj3)
+const objectManager = new ObjectManager(engine, camera, scene, obj1, obj2, obj3)
 objectManager.updateLayout();
 
 new HemisphericLight("light", new Vector3(0, 5, -5), scene);
--- src/utils/objectManager.ts
+++ src/utils/objectManager.ts
@@ -1,13 +1,15 @@
-import { Camera, Engine, Mesh } from "@babylonjs/core";
+import { BoundingBox, Camera, Engine, Mesh, Scene, Vector3 } from "@babylonjs/core";
 import * as constants from './constants';
 
 export class ObjectManager {
     engine: Engine;
+    scene: Scene;
     camera: Camera;
     objects: Array<Mesh>;
 
-    constructor(engine: Engine, camera: Camera, obj1: Mesh, obj2: Mesh, obj3: Mesh) {
+    constructor(engine: Engine, camera: Camera, scene: Scene, obj1: Mesh, obj2: Mesh, obj3: Mesh) {
         this.engine = engine;
+        this.scene = scene;
         this.camera = camera;
         this.objects = [obj1, obj2, obj3];
     }
@@ -21,11 +23,49 @@ export class ObjectManager {
         this.objects[1].position = layout.pos2;
         this.objects[2].position = layout.pos3;
 
-        const orthoSize = aspectRatio > 1 ? 3 : 3;
-        this.camera.orthoLeft = -orthoSize * aspectRatio;
-        this.camera.orthoRight = orthoSize * aspectRatio;
-        this.camera.orthoTop = orthoSize;
-        this.camera.orthoBottom = -orthoSize;
+        const boundingBox = new BoundingBox(new Vector3(), new Vector3());
+
+        this.objects.forEach(obj => {
+            const objBoundingBox = obj.getHierarchyBoundingVectors();
+            boundingBox.minimum.x = Math.min(boundingBox.minimum.x, objBoundingBox.min.x);
+            boundingBox.maximum.x = Math.max(boundingBox.maximum.x, objBoundingBox.max.x);
+
+            boundingBox.minimum.y = Math.min(boundingBox.minimum.y, objBoundingBox.min.y);
+            boundingBox.maximum.y = Math.max(boundingBox.maximum.y, objBoundingBox.max.y);
+
+            boundingBox.minimum.z = Math.min(boundingBox.minimum.z, objBoundingBox.min.z);
+            boundingBox.maximum.z = Math.max(boundingBox.maximum.z, objBoundingBox.max.z);
+        });
+
+        const boundingBoxSize = new Vector3(
+            boundingBox.maximum.x - boundingBox.minimum.x,
+            boundingBox.maximum.y - boundingBox.minimum.y,
+            boundingBox.maximum.z - boundingBox.minimum.z
+        );
+
+        // const orthoSize = 2;
+        // if (aspectRatio > 1) {
+        //     this.camera.orthoLeft = -orthoSize * aspectRatio;
+        //     this.camera.orthoRight = orthoSize * aspectRatio;
+        //     this.camera.orthoTop = orthoSize;
+        //     this.camera.orthoBottom = -orthoSize;
+        // } else {
+        //     this.camera.orthoLeft = -orthoSize;
+        //     this.camera.orthoRight = orthoSize;
+        //     this.camera.orthoTop = orthoSize * aspectRatio;
+        //     this.camera.orthoBottom = -orthoSize * aspectRatio;
+        // }
+        if (aspectRatio > 1) {
+            this.camera.orthoTop = (boundingBoxSize.y / 2)  * aspectRatio;
+            this.camera.orthoBottom = (-boundingBoxSize.y / 2)  * aspectRatio;
+            this.camera.orthoRight = (boundingBoxSize.x / 2) * aspectRatio;
+            this.camera.orthoLeft = (-boundingBoxSize.x / 2) * aspectRatio;
+        } else {
+            this.camera.orthoTop = boundingBoxSize.y / 2;
+            this.camera.orthoBottom = -boundingBoxSize.y / 2;
+            this.camera.orthoRight = boundingBoxSize.x / 2;
+            this.camera.orthoLeft = - boundingBoxSize.x / 2;
+        }
     }
 
     // setObjects(obj1: Mesh, obj2: Mesh, obj3: Mesh) {
