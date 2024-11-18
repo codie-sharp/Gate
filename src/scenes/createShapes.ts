b051bbed8dc0ce77de7644eb102aac65a3c637d6
Codie Sharp <codie.sharp@gmail.com>
2024-11-18 12:14:17 -0500

    global create scene object called from html, or event listeners in main?

--- src/scenes/createShapes.ts
+++ src/scenes/createShapes.ts
@@ -1,18 +1,28 @@
 import * as constants from '../utils/constants';
 import { Scene, MeshBuilder, Mesh, Vector3 } from '@babylonjs/core';
 
-export function createSphere(scene: Scene): Mesh {
-    const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 16 }, scene);
-    return sphere;
+class CreateShapes {
+    private scene: Scene;
+
+    constructor(scene: Scene) {
+        this.scene = scene;
+    }
+
+    createSphere(): Mesh {
+        const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 16 }, this.scene);
+        return sphere;
+    }
+    
+    createCube(): Mesh {
+        const cube = MeshBuilder.CreateBox("cube", { size: 2 }, this.scene);
+        return cube;
+    }
+    
+    createTorus(): Mesh {
+        const torus = MeshBuilder.CreateTorus("torus", { diameter: 1.75, thickness: .25 }, this.scene);
+        torus.rotation = new Vector3(constants.ninetyDegrees, 0, 0);
+        return torus;
+    }
 }
 
-export function createCube(scene: Scene): Mesh {
-    const cube = MeshBuilder.CreateBox("cube", { size: 2 }, scene);
-    return cube;
-}
-
-export function createTorus(scene: Scene): Mesh {
-    const torus = MeshBuilder.CreateTorus("torus", { diameter: 1.75, thickness: .25 }, scene);
-    torus.rotation = new Vector3(constants.ninetyDegrees, 0, 0);
-    return torus;
-}
+export default CreateShapes;
