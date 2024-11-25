c0421948ba685c1ed1063a101bab904b0badf3c7
Codie Sharp <codie.sharp@gmail.com>
2024-11-25 13:33:08 -0500

    node modules zip test. Uncommented code

--- src/scenes/Shapes.ts
+++ src/scenes/Shapes.ts
@@ -1,4 +1,4 @@
-// import earcut from "earcut";
+import earcut from "earcut";
 import { MeshBuilder, Mesh, Vector3, Scene, VertexData } from '@babylonjs/core';
 
 export const Shapes = [
@@ -10,37 +10,23 @@ export const Shapes = [
         return MeshBuilder.CreatePlane("square", { size: 2 }, scene);
     },
 
-    // function createTriangle(scene: Scene): Mesh {
-    //     const vertices = [new Vector3(0, 1, 0), new Vector3(-1, 0, 0), new Vector3(1, 0, 0)];
-    //     const triangle = MeshBuilder.CreatePolygon("triangle", { shape: vertices, sideOrientation: Mesh.DOUBLESIDE }, earcut, scene);
-    //     console.log(triangle);
-    //     return triangle;
-    // }
-
-    // function createTriangle(scene: Scene): Mesh {
-    //     const triangle = new Mesh("triangle", scene);
+    function createTriangle(scene: Scene): Mesh {
+        const triangle = new Mesh("triangle", scene);
     
-    //     const positions = [
-    //         0, 1, 0,
-    //         -1, -1, 0,
-    //         1, -1, 0
-    //     ];
+        const positions = [
+            0, 1, 0,
+            -1, -1, 0,
+            1, -1, 0
+        ];
     
-    //     const indices = earcut([0, 1, -1, -1, 1, -1], undefined, 2);
+        const indices = earcut([0, 1, -1, -1, 1, -1], undefined, 2);
     
-    //     const vertexData = new VertexData();
-    //     vertexData.positions = positions;
-    //     vertexData.indices = indices;
+        const vertexData = new VertexData();
+        vertexData.positions = positions;
+        vertexData.indices = indices;
     
-    //     vertexData.applyToMesh(triangle);
+        vertexData.applyToMesh(triangle);
     
-    //     return triangle;
-    // }
+        return triangle;
+    }
 ];
-
-// export function createShape(scene: Scene, position: Vector3): Mesh {
-//     const shapeFunction = shapes[Math.floor(Math.random() * shapes.length)];
-//     const shape = shapeFunction(scene);
-//     shape.position = position;
-//     return shape;
-// }
